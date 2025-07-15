a7d7eb80dd4e6604ea145a5e2ddd9567ccfb9c51
Mohdjariullah <112340610+Mohdjariullah@users.noreply.github.com>
2025-07-16 04:05:35 +0530

    Update page.tsx

---
+++ app/services/page.tsx
@@ -0,0 +1,11 @@
+import Footer from "@/components/common/Footer";
+import Navbar from "@/components/common/Navbar";
+
+export default function Services() {
+  return (
+    <div>
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
